7d4b5a1701b30cd3c6b9fd8199b565167f116f93
huhlim <huhlim@gmail.com>
2020-05-15 20:30:24 -0400

    fixed setup script error

--- bin/setup.py
+++ bin/setup.py
@@ -11,4 +11,4 @@ HOST_HOME = path.Dir(HOST_HOME, build=True)
 with JOBs_json.open("wt") as fout:
     fout.write(json.dumps([], indent=2))
 with HOSTs_json.open("wt") as fout:
-    fout.write(json.dumps([], indent=2))
+    fout.write(json.dumps({}, indent=2))
